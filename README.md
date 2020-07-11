0353c62939ac5ff541d90d0b51c3174e7205b68d
Richard Lee <richard.lee@education.gov.uk>
2020-07-11 13:38:28 +0100

    Number guessing game added

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rustme
 Basic Rust programming code snippets and documentation.
 See docs/ folder for more info.
+Source material comes from several on-line tutorials,
+the ubiquitous [Rust Programming Language](https://doc.rust-lang.org/book)
+and parts I have written myself.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::mem;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn value_types () { // this function returns an empty tuple, ie. -> ()
     // different integer types
@@ -11,8 +14,19 @@ fn value_types () { // this function returns an empty tuple, ie. -> ()
     println!("A 32bit integer indicated by u32/i32 takes {} bytes", mem::size_of_val(&unsigned_four));
     let signed_eight:i64 = 65;
     println!("A 64bit integer indicated by u64/i64 takes {} bytes", mem::size_of_val(&signed_eight));
+    let signed_sixteen:i128 = 65;
+    println!("A 128bit integer indicated by u128/i128 takes {} bytes", mem::size_of_val(&signed_sixteen));
     let unsigned_word:usize = 65;
     println!("A word is indicated by usize/isize and takes {} bytes", mem::size_of_val(&unsigned_word));
+
+    /*
+    ** You can enter literals as
+    ** Decimal	        dec = 98_222 // _ is beimg used as a separated, ie 98,222
+    ** Hex              hex = 0xff
+    ** Octal	        oct = 0o77
+    ** Binary           bin = 0b1111_0000 // again _ is an optional separator
+    ** Byte (u8 only)   byt = b'A'
+    */
 
     // floating point number can either be single or double precision
     let single_precision:f32 = 65.0;
@@ -80,7 +94,44 @@ fn std_ref_types() {
     // impl (object) will be covered later
 }
 
+fn guessing_game() {
+    const MIN_GUESS_VALUE: u32 = 1;
+    const MAX_GUESS_VALUE: u32 = 100;
+
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(MIN_GUESS_VALUE, MAX_GUESS_VALUE + 1);
+
+    loop {
+        println!("Please input your guess between {} and {}.", MIN_GUESS_VALUE, MAX_GUESS_VALUE);
+        let mut guess_in = String::new();
+
+        io::stdin()
+            .read_line(&mut guess_in)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess_in);
+
+        let guess: u32 = match guess_in.trim().parse() {
+            Ok(num) => num,
+            Err(_)  => {
+                println!("That was not a valid number");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
+
 fn main() {
     value_types();
     std_ref_types();
+    guessing_game();
 }
